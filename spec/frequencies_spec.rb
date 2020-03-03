cff51a4f012239fac276fb955dcf1269a78b82ce
root <hish.b92@gmail.com>
2020-03-03 17:06:39 +0000

    Feature: Method returns multiple inputs within each limit

--- spec/frequencies_spec.rb
+++ spec/frequencies_spec.rb
@@ -5,5 +5,8 @@ require 'frequencies'
       it "Given a set of sound waves, when the first frequency is passed, then the chosen input frequency should be returned within the lower and upper frequency limits" do
         expect(subject.soundwave([60],40,1000)).to eq [60]
       end
+      it "Given a set of sound waves, when two frequencies are passed, then the frequencies should be returned within the limits" do
+        expect(subject.soundwave([50,60],40,1000)).to eq [50,60]
+      end
     end
   end

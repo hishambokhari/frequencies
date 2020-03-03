52eb1bcb30a6fcafa96bc4d474dff97d41418272
root <hish.b92@gmail.com>
2020-03-03 17:30:36 +0000

    Refactor: Return upper_limit for inputs crossing the upper limit

--- lib/frequencies.rb
+++ lib/frequencies.rb
@@ -1,9 +1,13 @@
 class Frequencies
-  def soundwave(input,lower_limit,upper_limit)
-    if input == [1200]
-      [1000]
-    else
-      input
+  def soundwave(input, lower_limit, upper_limit)
+    new_inputs = input.map do |num|
+      if num > upper_limit
+        upper_limit
+      else
+        num
+      end
     end
+
+    return new_inputs
   end
 end
